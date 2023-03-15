8855e709bd6bb8f2b61ecea84ab42ce55df5e779
Lucas Viana <lucasvianamello@gmail.com>
2023-03-14 22:20:04 -0300

    [ATT] Weather-App Main

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,20 +4,33 @@ import local from "./Images/local.png";
 import wind from "./Images/wind.png";
 import humidity from "./Images/humidity.png";
 import rain from "./Images/rain.png";
+import clouds from "./Images/clouds.png";
 
 function weatherApp() {
   return (
-    <div className="App-content">
+    <div id="Card" className="App-content">
       <div className="Temperature-content">
+        <img className="Clouds" src={clouds} alt="Imagem Nuvens" />
         <header className="Localization">
           <img src={local} alt="Imagem Localização" />
           <p> Belém, Pará</p>
         </header>
+        <section>
+          <p className="Temperature">
+            <strong>18</strong>
+          </p>
+          <p className="Unidade">°C</p>
+          <p className="Sensacao">
+            22° <label>16°</label>
+          </p>
+        </section>
+
         <div className="Informations">
           <div className="Wind">
             <img src={wind} alt="Imagem Vento" />
-            <p>Vento</p>
             <p>
+              Vento
+              <br />
               <strong>
                 <b>17 </b>
               </strong>
@@ -26,26 +39,29 @@ function weatherApp() {
           </div>
           <div className="Humidity">
             <img src={humidity} alt="Imagem Umidade" />
-            <p>Umidade</p>
             <p>
+              Umidade
+              <br />
               <strong>
                 <b>17 </b>
               </strong>
-              Km/h
+              %
             </p>
           </div>
           <div className="Rain">
             <img src={rain} alt="Imagem Chuva" />
-            <p>Chuva</p>
             <p>
+              Chuva
+              <br />
               <strong>
                 <b>17 </b>
               </strong>
-              Km/h
+              %
             </p>
           </div>
         </div>
       </div>
+
       <div className="Air-content"></div>
       <div className="Sun-content"></div>
       <div className="Weather-content"></div>
